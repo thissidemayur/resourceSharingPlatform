8952f6106a0e79cfb83d7689001a7307b332daa1
Mayur Pal <thissidemayur@gmail.com>
2025-09-13 03:03:22 +0530

    feat(resource): implement resource route

---
+++ backend/src/routes/resource.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import {
+  createResource,
+  deleteResource,
+  getAllResources,
+  getSingleResource,
+  updateResource,
+} from '../controllers/resource.controller.js';
+import { authenticate } from '../middlewares/auth.middleware.js';
+const resourceRouter = Router();
+resourceRouter.post('/', authenticate, createResource); //create resoucrce
+resourceRouter.get('/', getAllResources); // get all resources
+resourceRouter.get('/:resourceId', getSingleResource); // get sungle resources
+resourceRouter.put('/:resourceId', authenticate, updateResource); // update resource
+resourceRouter.delete('/:resourceId', authenticate, deleteResource); // delete resource
+
+export { resourceRouter };
