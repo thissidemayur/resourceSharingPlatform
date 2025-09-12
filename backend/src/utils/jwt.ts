641406306eb7fabd36197cdb86925eeefb45f354
Mayur Pal <thissidemayur@gmail.com>
2025-09-13 03:13:37 +0530

    fix(jwt):use customJwtPayload type while verifyToken

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,11 +1,16 @@
+// utils/jwt.ts
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'supersecretkey';
+
+export interface CustomJwtPayload extends JwtPayload {
+  userId: string;
+}
 
 export const generateToken = (userId: string) => {
   return jwt.sign({ userId }, ACCESS_TOKEN_SECRET, { expiresIn: '7d' });
 };
 
-export const verifyToken = (token: string) => {
-  return jwt.verify(token, ACCESS_TOKEN_SECRET) as JwtPayload;
+export const verifyToken = (token: string): CustomJwtPayload => {
+  return jwt.verify(token, ACCESS_TOKEN_SECRET) as CustomJwtPayload;
 };
