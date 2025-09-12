85b3064f1637f6870ae166d33ae2421a7913718c
Mayur Pal <thissidemayur@gmail.com>
2025-09-13 03:08:25 +0530

    feat: implement resource route and cookie-parser middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { authRouter } from './routes/auth.route';
+import { authRouter } from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import { resourceRouter } from './routes/resource.routes.js';
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 app.use(cors({ origin: '*' }));
-
+app.use(cookieParser());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/resources', resourceRouter);
 
 const port = process.env.PORT || 4000;
 
